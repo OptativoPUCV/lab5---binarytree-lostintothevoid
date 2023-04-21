65c1429d4980116b591eff575204666d88f9d7b8
Replit user <>
2023-04-21 00:39:07 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -41,7 +41,7 @@ TreeMap * createTreeMap(int (*lower_than) (void* key1, void* key2)) {
   temp->root=NULL;
   temp->current=temp->root;
   temp->lower_than = lower_than;
-    return NULL;
+  return temp;
 }
 
 
